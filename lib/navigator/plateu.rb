644513c4c9a03682f26ec61f8b92e48b201c2c5f
Richie <richie@richie-laptop.(none)>
2010-01-12 01:25:46 +1030

    plateu: removed redundant code

--- lib/navigator/plateu.rb
+++ lib/navigator/plateu.rb
@@ -2,28 +2,10 @@ module Navigator
   class Plateu
 
     attr_accessor :length, :width  
-#    @length
-#    @width
    
     def initialize(messenger)
       @messenger = messenger
     end
-
-#    def length
-#      @length
-#    end
- 
-#   def set_length(length)
-#      @length = length
-#    end
-    
-#    def width
-#      @width
-#    end 
-
-#    def set_width(width) 
-#      @width = width
-#    end    
-
+   
   end
 end
